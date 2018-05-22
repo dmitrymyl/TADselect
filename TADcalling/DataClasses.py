694697ca9f428a78be21dd32551f3d479bfb0a76
dmitrymyl <dmitry@DESKTOP-ROL6I5L.localdomain>
2018-05-22 23:01:05 +0300

    Further development of GenomicRanges class.

--- TADcalling/DataClasses.py
+++ TADcalling/DataClasses.py
@@ -12,12 +12,13 @@ class GenomicRanges(object):
     """
     Basic class for any genomic ranges.
     """
-    # TODO: define input method. Maybe inheriting from np.array?
+    # TODO: implement load from bed file with 3 or 4 columns - make a function
 
-    def __init__(self, name, data_type='simulation', **kwargs):
+    def __init__(self, arr_object, arr_count=None, data_type='simulation', **kwargs):
         """
         Initialize base genomic ranges object.
-        :param name (:obj:`str`): specific name of given dataset
+        :param arr_object (:obj:`collection`): any array-like object
+        :param arr_count (:obj: `collection`): any array-like object of the same length as arr_object
         :param data_type (:obj: `str`): type of data (simulated segmentation (default), genomic track)
         :param kwargs:
             * assembly (:obj:`str`): name of assembly, default is '-'
@@ -25,10 +26,18 @@ class GenomicRanges(object):
             * size (:obj:`int`): size of dataset in bins, default is 0
             * resolution (:obj:`int`): size of bin in basepairs, default is 1000
             * metadata (:obj:`dict`): optional metadata dict, default is empty dict
-            Object of BaseCaller type or derivative has two attributes:
-            self._metadata -- metadata, fict with 'assembly', 'chr','resolution' keys
         """
-        self.name = name
+        self.data = np.array(arr_object, dtype=int, ndmin=2)
+        if self.data.shape[1] != 2:
+            raise Exception("Shape of arr_object is not n x 2")
+        self.length = self.data.shape[0]
+        if arr_count:
+            self.count = arr_count
+            if self.count.shape[0] != self.length:
+                raise Exception("Size of count array and amount of ranges are not equal.")
+        else:
+            # maybe fill with zeros?
+            self.count = np.full_like(self.data[:, 0], 1, dtype=int)
         self.data_type = data_type
         metadata = kwargs.get('metadata', {})
         if not isinstance(metadata, dict):
@@ -39,29 +48,33 @@ class GenomicRanges(object):
         metadata['size'] = kwargs.get('size', 0)
         metadata['resolution'] = metadata.get('resolution', 1000)
         self._metadata = metadata
-
-    def _TAD_bins(arr1, arr2):
+    
+    @staticmethod
+    def TAD_bins(arr):
         """
         Returns TADs as as objects from their coordinates.
         """
         _vector_str = np.vectorize(str)
-        return npchar.add(_vector_str(arr1), npchar.add(",", _vector_str(arr2)))
-
-    def _TAD_boundaries(arr1, arr2):
+        return npchar.add(_vector_str(arr[:, 0]), npchar.add(",", _vector_str(arr[:, 1])))
+    
+    @staticmethod
+    def TAD_boundaries(arr):
         """
         Returns TAD unique boundaries.
         """
-        return np.unique(np.append(arr1, arr2))
-
-    def _jaccard_index(arr1, arr2):
+        return np.unique(np.append(arr[:, 0], arr[:, 1]))
+    
+    @staticmethod
+    def jaccard_index(arr1, arr2):
         """
         Calculate Jaccard index between two 1-dimensional
         arrays containing genomic ranges.
         """
         intersection = np.isin(arr1, arr2)
         return sum(intersection) / (arr1.shape[0] + arr2.shape[0] - sum(intersection))
-
-    def _overlap_coef(arr1, arr2):
+    
+    @staticmethod
+    def overlap_coef(arr1, arr2):
         """
         Calculate Overlap coefficient between two 1-dimensional
         arrays containing genomic ranges.
@@ -69,7 +82,7 @@ class GenomicRanges(object):
         intersection = np.isin(arr1, arr2)
         return sum(intersection) / min(arr1.shape[0], arr2.shape[0])
 
-    # TODO: redefine coefs with np.intersect1d, np.union1d, np.setdiff1d. Set routines in numpy.
+    # Redefine coefs with np.intersect1d, np.union1d, np.setdiff1d? Set routines in numpy.
 
     def count_coef(self, other, coef="JI TADs"):
         """
@@ -80,28 +93,28 @@ class GenomicRanges(object):
         true segmentation obtained from simulation.
         """
         if coef == 'JI TADs':
-            return _jaccard_index(_TAD_bins(self[:, 0], self[:, 1]), _TAD_bins(other[:, 0], other[:, 1]))
+            return GenomicRanges.jaccard_index(GenomicRanges.TAD_bins(self.data), GenomicRanges.TAD_bins(other.data))
 
         elif coef == 'JI boundaries':
-            return jaccard_index(_TAD_boundaries(self[:, 0], self[:, 1]), _TAD_boundaries(other[:, 0], other[:, 1]))
+            return GenomicRanges.jaccard_index(GenomicRanges.TAD_boundaries(self.data), GenomicRanges.TAD_boundaries(other.data))
 
         elif coef == 'OC TADs':
-            return _overlap_coef(TAD_bins(self[:, 0], self[:, 1]), TAD_bins(other[:, 0], other[:, 1]))
+            return GenomicRanges.overlap_coef(GenomicRanges.TAD_bins(self.data), GenomicRanges.TAD_bins(other.data))
 
         elif coef == 'OC boundaries':
-            return _overlap_coef(_TAD_boundaries(self[:, 0], self[:, 1]), _TAD_boundaries(other[:, 0], other[:, 1]))
+            return GenomicRanges.overlap_coef(GenomicRanges.TAD_boundaries(self.data), GenomicRanges.TAD_boundaries(other.data))
 
         elif coef == 'TPR TADs':
-            return sum(np.isin(_TAD_bins(self), _TAD_bins(other))) / _TAD_bins(other).shape[0]
+            return sum(np.isin(GenomicRanges.TAD_bins(self.data), GenomicRanges.TAD_bins(other.data))) / GenomicRanges.TAD_bins(other.data).shape[0]
 
         elif coef == 'FDR TADs':
-            return sum(~np.isin(_TAD_bins(self), _TAD_bins(other))) / _TAD_bins(self).shape[0]
+            return sum(~np.isin(GenomicRanges.TAD_bins(self.data), GenomicRanges.TAD_bins(other.data))) / GenomicRanges.TAD_bins(self.data).shape[0]
 
         elif coef == 'TPR boundaries':
-            return sum(np.isin(_TAD_boundaries(self), _TAD_boundaries(other))) / _TAD_boundaries(other).shape[0]
+            return sum(np.isin(GenomicRanges.TAD_boundaries(self.data), GenomicRanges.TAD_boundaries(other.data))) / GenomicRanges.TAD_boundaries(other.data).shape[0]
 
         elif coef == 'FDR boundaries':
-            return sum(~np.isin(_TAD_boundaries(self), _TAD_boundaries(other))) / _TAD_boundaries(self).shape[0]
+            return sum(~np.isin(GenomicRanges.TAD_boundaries(self.data), GenomicRanges.TAD_boundaries(other.data))) / GenomicRanges.TAD_boundaries(self.data).shape[0]
 
         else:
             raise Exception('Coefficient not understood: {}'.format(coef))
@@ -109,4 +122,4 @@ class GenomicRanges(object):
     # TODO: define JI and OC for boundaries with offset as in @agal
     # TODO: count number of shared TADs.
     # TODO: implement bedtools intersect with 70% coverage. Easy in while loop
-    # by O(n+m), but want to to implement in numpy.
+    # by O(n+m), but want to implement in numpy.
