c407eeadbd42c74fb1f50681020582e6386469a9
Aleksandra Galitsyna <agalitzina@gmail.com>
2018-06-18 01:39:32 +0300

    InteractionMatrix class added. Multiple interaction matrix transformations implemented. Transfer of all data-specific processing steps to InteractionMatrix started. Logger fixed.

--- TADcalling/utils.py
+++ TADcalling/utils.py
@@ -6,7 +6,7 @@ import lavaburst
 import pandas as pd
 import time
 
-from .logger import logger
+from .logger import TADcalling_logger
 
 
 # Basic utils to run linux commands ###
@@ -16,17 +16,17 @@ def call_and_check_errors(command):
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             shell=True, executable='/bin/bash')
     (stdout, stderr) = proc.communicate()
-    logger.info("Check stdout: {}".format(stdout))
+    TADcalling_logger.info("Check stdout: {}".format(stdout))
     if stderr:
-        logger.info("Stderr is not empty. Might be an error in call_and_check_errors for the command: %s" % command)
-        logger.info("Check stderr: %s" % stderr)
+        TADcalling_logger.info("Stderr is not empty. Might be an error in call_and_check_errors for the command: %s" % command)
+        TADcalling_logger.info("Check stderr: %s" % stderr)
         return stderr
     else:
         return 0
 
 
 def run_command(command, force=False):
-    logger.info(command)
+    TADcalling_logger.info(command)
 
     possible_outfile = command.split('>')
 
@@ -34,15 +34,15 @@ def run_command(command, force=False):
         possible_outfile = possible_outfile[-1]
         if os.path.isfile(possible_outfile):
             if force:
-                logger.info("Outfile %s exists. It will be overwritten!" % possible_outfile)
+                TADcalling_logger.info("Outfile %s exists. It will be overwritten!" % possible_outfile)
             else:
-                logger.error("Outfile %s exists. Please, delete it, or use force=True to overwrite it."
-                             % possible_outfile)
+                TADcalling_logger.error("Outfile %s exists. Please, delete it, or use force=True to overwrite it."
+                                        % possible_outfile)
 
     cmd_bgn_time = time.time()
     is_err = call_and_check_errors(command)
     cmd_end_time = time.time()
 
-    logger.info("Command completed: %f" % (cmd_end_time - cmd_bgn_time))
+    TADcalling_logger.info("Command completed: %f" % (cmd_end_time - cmd_bgn_time))
 
     return is_err
